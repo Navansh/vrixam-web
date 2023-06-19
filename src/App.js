be4f18d94f4dd0a8e08ae060cd06a91f7c06cb47
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-06-20 00:40:03 +0530

    our motto

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import CarouselFade from "./components/CarouselFade";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Progress from "./components/Progress";
+import OurMotto from "./components/OurMotto";
 
 function App() {
   return (
     <div>
       <Navbar />
       <CarouselFade />
+      <OurMotto />
       <Progress></Progress>
     </div>
   );
--- src/components/CarouselFade.jsx
+++ src/components/CarouselFade.jsx
@@ -3,25 +3,28 @@ import carasol_data from "../data/Carasol-data";
 import Navbar from "./Navbar";
 function CarouselFade() {
   return (
-    <Carousel fade>
-      {carasol_data.map((item) => {
-        return (
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-img'
-              src={item.url}
-              alt='First slide'
-            />
-            <Carousel.Caption className='captions-all'>
-              <h3 className=" font-NoeDisplay">{item.title}</h3>
-              <p className=" font-PlusJakartaSans">{item.subtitle}</p>
-              <button className='support-btn font-AnekMalayalam'>Support Now</button>
-            </Carousel.Caption>
-          </Carousel.Item>
-        );
-      })}
-     
-    </Carousel>
+    <div className='hero-full'>
+      <Carousel fade interval={3000}>
+        {carasol_data.map((item) => {
+          return (
+            <Carousel.Item>
+              <img
+                className='d-block w-100 carousel-img'
+                src={item.url}
+                alt='First slide'
+              />
+              <Carousel.Caption className='captions-all'>
+                <h3 className=' font-NoeDisplay'>{item.title}</h3>
+                <p className=' font-PlusJakartaSans'>{item.subtitle}</p>
+                <button className='support-btn font-AnekMalayalam'>
+                  Support Now
+                </button>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 }
 
