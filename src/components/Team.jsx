eb7a6ca4a4bfd0622790793312f1237271d24859
Navansh <khandelwalnavansh1@gmail.com>
2023-06-21 00:57:28 +0530

    Added bg

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div className='max-w-[1440px] mx-auto flex flex-col items-center'>
-        <div className='mt-[5rem]'>
-            <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687272782/team_desktop_xvzurg.png" alt="" loading='lazy'/>
+    <div className=' cssteam'>
+        <div className='max-w-[1440px] mx-auto flex flex-col items-center'>
+            <div className='mt-[5rem]'>
+                <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687288561/team_2_j1xaid.png" alt="" loading='lazy'/>
+            </div>
+            <div className=' mt-[2.75rem] max-w-[383px] rounded-full cssbutton flex gap-2 justify-center items-center bg-[#E6F0EA]'>
+                <p className=' mb-0 uppercase text-[#006C2C] font-AnekMalayalam text-[18px] leading-[25px]'>view list of contributors</p>
+                <img src='https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687274934/Vector_xsvnrd.svg' alt='d'/>
+            </div>
         </div>
-        <div className=' mt-[2.75rem] max-w-[383px] rounded-full cssbutton flex gap-2 justify-center items-center bg-[#E6F0EA]'>
-            <p className=' mb-0 uppercase text-[#006C2C] font-AnekMalayalam text-[18px] leading-[25px]'>view list of contributors</p>
-            <img src='https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687274934/Vector_xsvnrd.svg' alt='d'/>
-        </div>
+        
     </div>
   )
 }
