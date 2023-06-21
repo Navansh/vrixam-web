33723549bd90cf4b540b18ddf458d9f560d22c56
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-06-21 18:00:49 +0530

    motto and hero

--- src/components/CarouselFade.jsx
+++ src/components/CarouselFade.jsx
@@ -4,7 +4,7 @@ import { BiDonateHeart } from "react-icons/bi";
 function CarouselFade() {
   return (
     <div className='hero-full'>
-      <Carousel fade interval={4000}>
+      <Carousel fade interval={13000}>
         {carasol_data.map((item) => {
           return (
             <Carousel.Item>
@@ -14,18 +14,20 @@ function CarouselFade() {
                 alt='First slide'
               />
               <Carousel.Caption className='captions-all'>
-                <h3 className=' font-NoeDisplay' style={item.long}>
-                  {item.title}
-                </h3>
-                <p className=' font-PlusJakartaSans' style={item.short}>
-                  {item.subtitle}
-                </p>
-                <button className='support-btn hover:cursor-pointer items-center font-AnekMalayalam rounded-full'>
-                  Support Now
-                  <span>
-                    <BiDonateHeart className=' mb-1'></BiDonateHeart>
-                  </span>
-                </button>
+                <div className='captions-inner'>
+                  <h3 className=' font-NoeDisplay' style={item.long}>
+                    {item.title}
+                  </h3>
+                  <p className=' font-PlusJakartaSans' style={item.short}>
+                    {item.subtitle}
+                  </p>
+                  <button className='support-btn hover:cursor-pointer items-center font-AnekMalayalam rounded-full'>
+                    Support Now
+                    <span>
+                      <BiDonateHeart className=' mb-1'></BiDonateHeart>
+                    </span>
+                  </button>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
           );
