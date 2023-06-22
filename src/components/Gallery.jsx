c9501d82d0797e7d44b321d68a1b04999b31ca40
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-06-22 11:43:52 +0530

    gallery-sm-optimised

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,16 +6,12 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 1000 },
+    breakpoint: { max: 4000, min: 768 },
     items: 2,
   },
 
   tablet: {
-    breakpoint: { max: 1000, min: 600 },
-    items: 1,
-  },
-  mobile: {
-    breakpoint: { max: 600, min: 0 },
+    breakpoint: { max: 768, min: 0 },
     items: 1,
   },
 };
@@ -49,7 +45,7 @@ const Gallery = () => {
         <h3>Our Gallery</h3>
       </div>
       <Carousel
-        className='carousel-gallery'
+        className='carousel-gallery car-1'
         swipeable={true}
         arrows={false}
         renderButtonGroupOutside={true}
@@ -59,6 +55,105 @@ const Gallery = () => {
         responsive={responsive}
         infinite={true}
         slidesToSlide={2}
+      >
+        <div className=' why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-1.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+
+        <div className='why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-2.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+        <div className='why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-3.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+        <div className='why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-4.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+
+        <div className=' why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-5.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+
+        <div className=' why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-6.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+
+        <div className='why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-7.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+
+        <div className='why-cards'>
+          <div className='card-img'>
+            <img src='../assets/Gallery-8.png' alt='' />
+          </div>
+          {/* <div className='card-container'>
+          <p>
+            
+          </p>
+        </div> */}
+        </div>
+      </Carousel>
+
+      <Carousel
+        className='carousel-gallery car-2'
+        swipeable={true}
+        arrows={false}
+        renderButtonGroupOutside={true}
+        customButtonGroup={<ButtonGroup />}
+        draggable={false}
+        autoPlay={true}
+        responsive={responsive}
+        infinite={true}
+        slidesToSlide={1}
       >
         <div className=' why-cards'>
           <div className='card-img'>
