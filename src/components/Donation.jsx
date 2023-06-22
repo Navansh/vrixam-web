a07bee4a7a9bf4d6481c92f63c57feb643052c87
Navansh <khandelwalnavansh1@gmail.com>
2023-06-22 15:25:47 +0530

    made donation and utilze sections responsive

--- src/components/Donation.jsx
+++ src/components/Donation.jsx
@@ -1,55 +1,270 @@
-import React from 'react'
+import React from "react";
 
 const Donation = () => {
   return (
-    <div className=' cssdonation w-full' id='donate'>
-        <div className=' flex max-w-[1440px] mx-auto'>
-            <div className='flex flex-col ml-[6.75rem] mt-[3.875rem] '>
-                <div className=' flex items-center'>
-                    <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687167490/logo_m3yaf9.png" alt="" className=' h-[87px] w-[87px] mr-[19.44px]'/>
-                    <div className=''>
-                        <p className=' text-[35px] text-white leading-[32px] font-NoeStandard mb-0'>Vrixam</p>
-                        <p className=' text-[35px] leading-[32px] text-white font-NoeStandard mb-0'>Amritam</p>
-                        <p className=' text-[19px] leading-[26px] uppercase mb-0 text-white'>Seva Sansthan</p>
-                    </div>
-                </div>
-
-                <div className=' mt-[46px] mb-0 text-white text-[42px] font-NoeStandard max-w-[483px] leading-[57px]'>
-                    <p>We're a nonprofit with the vision of building a better future.</p>
-                </div>
-
+    <div className=" cssdonation w-full" id="donate">
+      <div className=" flex max-w-[1440px] mx-auto sm:hidden lg:flex">
+        <div className="flex flex-col ml-[6.75rem] mt-[3.875rem] ">
+          <div className=" flex items-center">
+            <img
+              src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687167490/logo_m3yaf9.png"
+              alt=""
+              className=" h-[87px] w-[87px] mr-[19.44px]"
+            />
+            <div className="">
+              <p className=" text-[35px] text-white leading-[32px] font-NoeStandard mb-0">
+                Vrixam
+              </p>
+              <p className=" text-[35px] leading-[32px] text-white font-NoeStandard mb-0">
+                Amritam
+              </p>
+              <p className=" text-[19px] leading-[26px] uppercase mb-0 text-white">
+                Seva Sansthan
+              </p>
             </div>
+          </div>
 
-            <div className=' ml-[6.375rem] mb-[5.375rem] mt-[4.688rem]'>
-                <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687266701/upiqr_kuxznp.png" alt="" />
-            </div>
-
-            <div className='flex flex-col ml-[4rem] mt-[6.375rem] max-w-[325px]'>
-                <div className='flex flex-col gap-[1.467rem]'>
-                    <div>
-                        <p className=' mb-0 font-AnekMalayalam  text-[20px] text-white leading-[28px] opacity-80'>MERCHANT NAME</p>
-                        <p className=' mb-0 font-PlusJakartaSans text-white text-[24px] leading-[33px]'>VRIXAM AMRITAM SEVA SANSTHAN</p>
-                    </div>
-                    
-                    <div className=' flex flex-col'>
-                        <p className=' mb-0 font-AnekMalayalam text-[20px] text-white leading-[28px] uppercase opacity-80'>upi id</p>
-                        <p className=' mb-0 font-PlusJakartaSans text-white text-[24px] leading-[33px]'>SANSTHANVRIXAM@SBI</p>
-                    </div>
-                </div>
-
-                <div className=' mt-[3.667rem] flex flex-col gap-[1rem]'>
-                    <p className=' text-white font-AnekMalayalam text-[18px] leading-[25px] opacity-80 mb-0'>We support secure transactions through</p>
-                    <div className='flex items-center gap-[1rem]'>
-                        <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/UPI-Logo-vector_1_upr5je.png" className=' w-[69px] h-[24px]' alt="" />
-                        <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/Paytm_Logo__standalone_1_fttc5r.png" alt=" " className='w=[79px] h-[24px]' />
-                        <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/google-pay-icon_1_bujlpn.png" alt="" className=' w-[33px] h-[28px]'/>
-                        <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/phonepe-1_1_yduhcu.png" alt="" className=' w-[35px] h-[35px]'/>
-                    </div>
-                </div>
-            </div>
+          <div className=" mt-[46px] mb-0 text-white text-[42px] font-NoeStandard max-w-[483px] leading-[57px]">
+            <p>
+              We're a nonprofit with the vision of building a better future.
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Donation
+        <div className=" ml-[6.375rem] mb-[5.375rem] mt-[4.688rem]">
+          <img
+            src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687266701/upiqr_kuxznp.png"
+            alt=""
+          />
+        </div>
+
+        <div className="flex flex-col ml-[4rem] mt-[6.375rem] max-w-[325px]">
+          <div className="flex flex-col gap-[1.467rem]">
+            <div>
+              <p className=" mb-0 font-AnekMalayalam  text-[20px] text-white leading-[28px] opacity-80">
+                MERCHANT NAME
+              </p>
+              <p className=" mb-0 font-PlusJakartaSans text-white text-[24px] leading-[33px]">
+                VRIXAM AMRITAM SEVA SANSTHAN
+              </p>
+            </div>
+
+            <div className=" flex flex-col">
+              <p className=" mb-0 font-AnekMalayalam text-[20px] text-white leading-[28px] uppercase opacity-80">
+                upi id
+              </p>
+              <p className=" mb-0 font-PlusJakartaSans text-white text-[24px] leading-[33px]">
+                SANSTHANVRIXAM@SBI
+              </p>
+            </div>
+          </div>
+
+          <div className=" mt-[3.667rem] flex flex-col gap-[1rem]">
+            <p className=" text-white font-AnekMalayalam text-[18px] leading-[25px] opacity-80 mb-0">
+              We support secure transactions through
+            </p>
+            <div className="flex items-center gap-[1rem]">
+              <img
+                src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/UPI-Logo-vector_1_upr5je.png"
+                className=" w-[69px] h-[24px]"
+                alt=""
+              />
+              <img
+                src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/Paytm_Logo__standalone_1_fttc5r.png"
+                alt=" "
+                className="w=[79px] h-[24px]"
+              />
+              <img
+                src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/google-pay-icon_1_bujlpn.png"
+                alt=""
+                className=" w-[33px] h-[28px]"
+              />
+              <img
+                src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/phonepe-1_1_yduhcu.png"
+                alt=""
+                className=" w-[35px] h-[35px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-[768px] mx-auto sm:hidden md:flex lg:hidden">
+        <div className="w-192 h-[679.40px] px-12 py-16 flex-col justify-start items-start gap-14 inline-flex">
+          <div className="self-stretch p-[0px] justify-start items-start gap-6 inline-flex">
+            <div className="self-stretch p-[0px] flex-col justify-between items-start gap-12 inline-flex">
+              <div className="p-[0px] flex-col justify-start items-start gap-12 flex">
+                <div className="relative">
+                  <div className=" flex items-center">
+                    <img
+                      src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687167490/logo_m3yaf9.png"
+                      alt=""
+                      className=" h-[87px] w-[87px] mr-[19.44px]"
+                    />
+                    <div className="">
+                      <p className=" text-[35px] text-white leading-[32px] font-NoeStandard mb-0">
+                        Vrixam
+                      </p>
+                      <p className=" text-[35px] leading-[32px] text-white font-NoeStandard mb-0">
+                        Amritam
+                      </p>
+                      <p className=" text-[19px] leading-[26px] uppercase mb-0 text-white">
+                        Seva Sansthan
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-[362px] font-NoeStandard  text-white text-[36px] font-bold leading-10">
+                  We're a nonprofit with the vision of building a better future.
+                </div>
+              </div>
+              <div className=" mt-[3.667rem] flex flex-col gap-[1rem]">
+                <p className=" text-white font-AnekMalayalam text-[18px] leading-[25px] opacity-80 mb-0">
+                  We support secure transactions through
+                </p>
+                <div className="flex items-center gap-[1rem]">
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/UPI-Logo-vector_1_upr5je.png"
+                    className=" w-[69px] h-[24px]"
+                    alt=""
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/Paytm_Logo__standalone_1_fttc5r.png"
+                    alt=" "
+                    className="w=[79px] h-[24px]"
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/google-pay-icon_1_bujlpn.png"
+                    alt=""
+                    className=" w-[33px] h-[28px]"
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/phonepe-1_1_yduhcu.png"
+                    alt=""
+                    className=" w-[35px] h-[35px]"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="p-[0px] flex-col justify-start items-start gap-14 inline-flex">
+              <div className="w-[247.22px] h-[329.22px]">
+                <img
+                  src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687266701/upiqr_kuxznp.png"
+                  alt=""
+                />
+              </div>
+              <div className="p-[0px] flex-col justify-start items-start gap-[12.33px] flex">
+                <div className="p-[0px] flex-col justify-start items-start gap-[1.92px] flex">
+                  <div className="opacity-80 text-center text-white text-[18px] font-medium uppercase">
+                    Merchant Name
+                  </div>
+                  <div className="w-[325px] text-white text-[24px] font-bold">
+                    VRIXAM AMRITAM SEVA SANSTHAN
+                  </div>
+                </div>
+                <div className="p-[0px] flex-col justify-start items-start gap-[1.92px] flex">
+                  <div className="opacity-80 text-center text-white text-[18px] font-medium uppercase">
+                    upi id
+                  </div>
+                  <div className="w-[325px] text-white text-[24px] font-bold">
+                    SANSTHANVRIXAM@SBI
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:flex md:hidden max-w-[368px] mx-auto">
+        <div className="w-[360px] h-[1066.60px] px-6 py-16 flex-col justify-start items-start gap-14 inline-flex">
+          <div className="self-stretch h-[938.60px] p-[0px] flex-col justify-start items-start gap-12 flex">
+            <div className="self-stretch h-[212.75px] p-[0px] flex-col justify-start items-center gap-12 flex">
+              <div className="relative">
+                    <div className=" flex items-center">
+                        <img
+                        src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687167490/logo_m3yaf9.png"
+                        alt=""
+                        className=" h-[87px] w-[87px] mr-[19.44px]"
+                        />
+                        <div className="">
+                        <p className=" text-[35px] text-white leading-[32px] font-NoeStandard mb-0">
+                            Vrixam
+                        </p>
+                        <p className=" text-[35px] leading-[32px] text-white font-NoeStandard mb-0">
+                            Amritam
+                        </p>
+                        <p className=" text-[19px] leading-[26px] uppercase mb-0 text-white">
+                            Seva Sansthan
+                        </p>
+                        </div>
+                    </div>
+              </div>
+              <div className="self-stretch text-center text-white text-[28px] font-bold font-NoeStandard leading-9">
+                We're a nonprofit with the vision of building a better future.
+              </div>
+            </div>
+            <div className="self-stretch h-[564.66px] p-[0px] flex-col justify-start items-start gap-12 flex">
+              <div className=" pb-10 bg-white rounded-2xl ml-6 flex-col justify-start items-center gap-10 flex">
+                <div className="w-[247.22px] h-[329.22px]">
+                    <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687266701/upiqr_kuxznp.png"
+                    alt=""
+                    />
+                </div>
+              </div>
+              <div className="self-stretch h-28 p-[0px] flex-col mr-4 justify-start items-start gap-[22.33px] flex">
+                <div className="self-stretch h-[44.92px] p-[0px] flex-col justify-start items-start gap-[9.92px] flex">
+                  <div className="self-stretch opacity-80 text-center text-white text-[15px] font-medium uppercase">
+                    Merchant Name
+                  </div>
+                  <div className="self-stretch text-center text-white text-[18px] font-bold">
+                    VRIXAM AMRITAM SEVA SANSTHAN
+                  </div>
+                </div>
+                <div className="self-stretch mt-3 h-[44.92px] p-[0px] flex-col justify-start items-start gap-[9.92px] flex">
+                  <div className="self-stretch opacity-80 text-center text-white text-[15px] font-medium uppercase">
+                    upi id
+                  </div>
+                  <div className="self-stretch text-center text-white text-[18px] font-bold">
+                    SANSTHANVRIXAM@SBI
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div className=" mt-[3.667rem] flex flex-col gap-[1rem]">
+                <p className=" text-white font-AnekMalayalam text-[18px] leading-[25px] opacity-80 mb-0">
+                  We support secure transactions through
+                </p>
+                <div className="flex ml-3 items-center gap-[1rem]">
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/UPI-Logo-vector_1_upr5je.png"
+                    className=" w-[69px] h-[24px]"
+                    alt=""
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/Paytm_Logo__standalone_1_fttc5r.png"
+                    alt=" "
+                    className="w=[79px] h-[24px]"
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/google-pay-icon_1_bujlpn.png"
+                    alt=""
+                    className=" w-[33px] h-[28px]"
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687268676/phonepe-1_1_yduhcu.png"
+                    alt=""
+                    className=" w-[35px] h-[35px]"
+                  />
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Donation;
--- src/components/Utilize.jsx
+++ src/components/Utilize.jsx
@@ -5,34 +5,34 @@ const Utilize = () => {
     <div className=' w-full  cssutilize'>
         <div className=' flex sm:flex-col lg:flex-row mx-auto py-[70px] gap-9 max-w-[1440px]'>
             <div className=' max-w-[561px] flex flex-col md:items-center lg:items-stretch gap-[14px] md:ml-[48px] mx-auto lg:ml-[180px] mt-[21px]'>
-                <p className=' font-NoeStandard mb-0 sm:text-center lg:text-left text-[36px] leading-[49px] text-[#04461F]'>How we utilize your <br /> donations and where it goes</p>
-                <p className=' font-PlusJakartaSans sm:text-center lg:text-left text-[16px] leading-[27px] text-[#2D2D2D] mb-0'>We understand that when you make a donation, you <br /> want to know exactly where your money is going and we <br /> pledge to be transparent.</p>
+                <p className=' font-NoeStandard mb-0 sm:text-center lg:text-left sm:text-[28px] md:text-[36px]  leading-[49px] text-[#04461F]'>How we utilize your <br /> donations and where it goes</p>
+                <p className=' font-PlusJakartaSans sm:text-center sm:text-[13px] lg:text-left md:text-[16px] leading-[27px] text-[#2D2D2D] mb-0'>We understand that when you make a donation, you <br /> want to know exactly where your money is going and we <br /> pledge to be transparent.</p>
 
-                <div className='lg:hidden md:mt-[72px]'>
+                <div className='lg:hidden md:mt-[72px] sm:scale-75 md:scale-100 sm:ml-3 md:ml-0'>
                     <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687248186/utilize_qgswhh.png" className=' w-[512px] h-[313px]' alt="" />
                 </div>
 
-                <div className=' flex flex-wrap gap-6 mt-3'>
+                <div className=' flex flex-wrap sm:ml-7 sm:pl-8  md:pl-0 md:ml-[50px] lg:ml-0 sm:max-w-[412px] md:max-w-[672px] md:pt-4 gap-6 mt-3'>
                     <div className=' flex justify-center gap-[6px]'>
-                        <div className='h-[14px] mt-1 w-[14px] bg-[#006C2C]'></div>
-                        <p className=' mb-0'>Plantation Projects</p>
+                        <div className=' sm:h-[10px] sm:w-[10px] mt-1 md:h-[14px] md:w-[14px] bg-[#006C2C]'></div>
+                        <p className=' mb-0 sm:text-[13px] md:text-[16px]'>Plantation Projects</p>
                     </div>
                     <div className=' flex ml-5 justify-center gap-[6px]'>
-                        <div className='h-[14px] mt-1 w-[14px] bg-[#F2994A]'></div>
-                        <p className=' mb-0'>Conservation of Temples</p>
+                        <div className='h-[14px] mt-1 w-[14px] sm:h-[10px] sm:w-[10px] md:h-[14px] md:w-[14px] bg-[#F2994A]'></div>
+                        <p className=' mb-0 sm:text-[13px] md:text-[16px]'>Conservation of Temples</p>
                     </div>
                     <div className=' flex justify-center gap-[6px]'>
-                        <div className='h-[14px] mt-1 w-[14px] bg-[#54C1F0]'></div>
-                        <p className=' mb-0'>Rainwater Harvesting</p>
+                        <div className='h-[14px] mt-1 w-[14px] sm:h-[10px] sm:w-[10px] md:h-[14px] md:w-[14px] bg-[#54C1F0]'></div>
+                        <p className=' mb-0 sm:text-[13px] md:text-[16px]'>Rainwater Harvesting</p>
                     </div>
                     <div className=' flex justify-center gap-[6px]'>
-                        <div className='h-[14px] mt-1 w-[14px] bg-[#F2C94C]'></div>
-                        <p className=' mb-0'>Solar Energy Panel Setups</p>
+                        <div className='h-[14px] mt-1 w-[14px] sm:h-[10px] sm:w-[10px] md:h-[14px] md:w-[14px] bg-[#F2C94C]'></div>
+                        <p className=' mb-0 sm:text-[13px] md:text-[16px]'>Solar Energy Panel Setups</p>
                     </div>
                 </div>
             </div>
 
-            <div className=' ml-5  md:hidden lg:inline'>
+            <div className=' ml-5  sm:hidden lg:inline'>
                 <img src="https://res.cloudinary.com/dsnnvjuqz/image/upload/v1687248186/utilize_qgswhh.png" className=' w-[512px] h-[313px]' alt="" />
             </div>
         </div>
